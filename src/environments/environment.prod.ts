5e33aa640a10b937c6cb7403ed9c13e883bc270d
Leszek Błażewski <blazewski.leszek@gmail.com>
2020-01-27 11:36:47 +0100

    Add firebase conifg

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,15 @@
 export const environment = {
   production: true,
   omdApiUrl: 'http://www.omdbapi.com/',
-  apiKey: '3f1e8c78'
+  apiKey: '3f1e8c78',
+  firebaseConfig: {
+    apiKey: "APIKEY",
+    authDomain: "filmsearch-efb50.firebaseapp.com",
+    databaseURL: "https://filmsearch-efb50.firebaseio.com",
+    projectId: "filmsearch-efb50",
+    storageBucket: "filmsearch-efb50.appspot.com",
+    messagingSenderId: "749647439153",
+    appId: "1:749647439153:web:32b24c7a8ef53c612e3c10",
+    measurementId: "G-T7L1CN9CXQ"
+  }
 };
